e14f4310e15d8b3c9215f7d02085f1ec7c98a82b
Arun-mec <arunalakkalpalla@gmail.com>
2022-07-22 21:02:05 +0530

    Final version of the code

--- helpers/page-helpers.js
+++ helpers/page-helpers.js
@@ -1,7 +1,7 @@
 var db=require('../config/connection')
 var collection=require('../config/collections')
 var objectId= require('mongodb').ObjectId
-const { response } = require('./app')
+const { response } = require('../app')
 
 module.exports={
     addArticle:(blogDetails)=>{
