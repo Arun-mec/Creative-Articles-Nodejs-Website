b1ab36fa9f919441dd92ac1d1c893d97054a5061
Arun-mec <arunalakkalpalla@gmail.com>
2022-07-22 17:29:45 +0530

    Final version of the code

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var userHelpers = require('./helpers/user-helpers')
+var userHelpers = require('../helpers/user-helpers')
 const { application } = require('express');
 
 
